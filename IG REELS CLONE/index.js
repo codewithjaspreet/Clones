749613769d16ccf0399dbe0e21ca1bdee3748bf1
jaspreetSingh <jaspreetsodhi02@gmail.com>
2021-06-25 09:55:18 +0530

    Intial Commit

---
+++ IG REELS CLONE/index.js
@@ -0,0 +1,13 @@
+const videos  = document.querySelectorAll(video);
+
+for(const video of videos){
+    video.addEventListener('click',function(){
+
+        if(video.paused){
+            video.play();
+        }
+        else{
+            video.pause();
+        }
+    });
+}
